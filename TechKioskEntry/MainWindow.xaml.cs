ef839bd6204b8b10de998b9d026a4ec39d2d8347
Terry Holmes <tchaser3@outlook.com>
2024-12-24 07:15:44 -0500

    Add project files.

---
+++ TechKioskEntry/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewEventLogDLL;
+using DataValidationDLL;
+using WorkTaskStatsDLL;
+using ProductivityDataEntryDLL;
+using DateSearchDLL;
+using EmployeeDateEntryDLL;
+using ProjectMatrixDLL;
+using NewEmployeeDLL;
+using ProjectsDLL;
+using WorkTaskDLL;
+
+namespace TechKioskEntry
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void expClose_Expanded(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
